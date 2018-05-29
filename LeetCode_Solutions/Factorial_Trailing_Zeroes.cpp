aa4b84347caf36c875387cf1efc83abd7e2a94c0
sayands <sayandsarkar.1997@gmail.com>
2018-05-29 12:06:13 +0300

    Daily commit II Leetcode

---
+++ LeetCode_Solutions/Factorial_Trailing_Zeroes.cpp
@@ -0,0 +1,13 @@
+// 172 - Factorial Trailing Zeroes
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+  public:
+    int trailingZeroes(int n)
+    {
+        return n == 0 ? 0 : n / 5 + trailingZeroes(n / 5);
+    }
+};
