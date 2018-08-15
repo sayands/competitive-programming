e057a6581e5f644f8734abf6c28c770ebe31dc44
sayands <sayandsarkar.1997@gmail.com>
2018-08-15 20:48:08 +0530

    Commit Leetcode interview prep

---
+++ LeetCode_Solutions/Group_Anagrams.cpp
@@ -0,0 +1,41 @@
+// 49 - Group Anagrams
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+  public:
+    vector<vector<string>> groupAnagrams(vector<string> &strs)
+    {
+        unordered_map<string, multiset<string>> mp;
+        for (string s : strs)
+        {
+            string t = strSort(s);
+            //sort(t.begin(), t.end());
+            mp[t].insert(s);
+        }
+
+        vector<vector<string>> anagrams;
+        for (auto m : mp)
+        {
+            vector<string> anagram(m.second.begin(), m.second.end());
+            anagrams.push_back(anagram);
+        }
+        return anagrams;
+    }
+
+  private:
+    string strSort(string &s)
+    {
+        int count[26] = {0}, n = s.length();
+        for (int i = 0; i < n; i++)
+            count[s[i] - 'a']++;
+        int p = 0;
+        string t(n, 'a');
+        for (int j = 0; j < 26; j++)
+            for (int i = 0; i < count[j]; i++)
+                t[p++] += j;
+        return t;
+    }
+};
